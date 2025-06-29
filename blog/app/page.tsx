461305e7c26018b800cbc1e946e771b831450cef
Dmytrii Tamurov <tamurov@sheridancollege.ca>
2025-06-29 13:14:49 -0400

    proj whole update 1

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">Dmytrii Tamurov`s Site</h1>
+        <h1 className="text-4xl font-bold mb-4">Dmytrii Tamurov Site</h1>
         <p className="text-xl text-gray-600">🚧 This website is currently in development. Stay tuned!</p>
       </div>
     </main>
